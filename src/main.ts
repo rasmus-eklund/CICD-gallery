547b392327327f2082b920ebd21af5e977cd41ab
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-06-13 22:52:15 +0200

    css drop clickable no?

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { fetchImage } from './fetcher.ts';
 import { renderPhoto, renderSearch } from './ui.ts';
 import { addSearch } from './search_storage';
 import { Photos } from 'unsplash-js/dist/methods/search/types/response';
+import searchLogo from './searchLogo.svg';
 
+const image = document.querySelector('.input-border__img') as HTMLImageElement;
+image.src = searchLogo;
 type State = {
   data: Photos;
   count: number;
@@ -17,7 +20,7 @@ const update = (newState: State) => {
   window.dispatchEvent(new Event('statechange'));
 };
 
-window.addEventListener('popstate', (event) => {
+window.addEventListener('popstate', event => {
   update(event.state);
   console.log(`Updated state ${event.state.count}`);
 });
--- src/ui.ts
+++ src/ui.ts
@@ -1,49 +1,62 @@
-import { type Photos } from 'unsplash-js/dist/methods/search/types/response'
-import { getSearch } from './search_storage'
+import { type Photos } from 'unsplash-js/dist/methods/search/types/response';
+import { getSearch } from './search_storage';
 
-const container = document.querySelector('.main__section') as HTMLElement
+const container = document.querySelector('.main__section') as HTMLElement;
 
 // const renderPhoto = (photos: Photos) => {
 const renderPhoto = (photos: Photos): void => {
-  clear()
-  const element = document.getElementsByTagName('template')[0]
-  const template = element.content
+  clear();
+  const element = document.getElementsByTagName('template')[0];
+  const template = element.content;
   const cardTemplate = template.querySelector(
     '.section__card'
-  ) as HTMLDivElement
+  ) as HTMLDivElement;
 
   photos.results.slice(0, 9).forEach(photo => {
-    const card = cardTemplate.cloneNode(true) as HTMLDivElement
-    const img = card.querySelector('.inner__front') as HTMLImageElement
-    const author = card.querySelector('.card__author') as HTMLSpanElement
+    const card = cardTemplate.cloneNode(true) as HTMLDivElement;
+    const img = card.querySelector('.inner__front') as HTMLImageElement;
+    const author = card.querySelector('.card__author') as HTMLSpanElement;
     const backP = card.querySelector(
       '.back__description'
-    ) as HTMLParagraphElement
-    img.src = photo.urls.small
+    ) as HTMLParagraphElement;
+    img.src = photo.urls.small;
     if (photo.alt_description !== null) {
-      img.setAttribute('alt', photo.alt_description)
+      img.setAttribute('alt', photo.alt_description);
     }
-    backP.textContent = photo.description
-    author.textContent = photo.user.name
-    container.append(card)
-  })
-}
+    backP.textContent = photo.description;
+    author.textContent = photo.user.name;
+    container.append(card);
+  });
+};
 
 const renderSearch = (): void => {
-  const data = getSearch()
+  const data = getSearch();
   if (data.length > 0) {
+    const input = document.querySelector(
+      '.input-border__input'
+    ) as HTMLInputElement;
+
     const el = document.querySelector(
       '.search-history__list'
-    ) as HTMLUListElement
-    const content = data.map(item => `<li>${item}</li>`).join('')
-    el.innerHTML = content
+    ) as HTMLUListElement;
+    const content = data
+      .map(item => `<li class="list__item">${item}</li>`)
+      .join('');
+    el.innerHTML = content;
+    const listItems = el.querySelectorAll('li');
+    listItems.forEach(item => {
+      item.addEventListener('click', () => {
+        input.value = item.textContent as string;
+        console.log(item.textContent);
+      });
+    });
   }
-}
+};
 
 const clear = (): void => {
   while (container.lastChild != null) {
-    container.lastChild.remove()
+    container.lastChild.remove();
   }
-}
+};
 
-export { renderPhoto, renderSearch }
+export { renderPhoto, renderSearch };
